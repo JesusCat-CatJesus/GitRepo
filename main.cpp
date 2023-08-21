074d8bd51a45b5e6f9e7eae43681f52cf261cd63
jesus cat | cat jesus ! <m.a.j.i.c.a.l.h.e.r.o.c.a.t@gmail.com>
2023-08-21 11:13:13 +0330

    made a dellay in program and added run file

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <thread>
 int main () {
     std::cout<<"dassax";
     std::cout<<"karides";
     std::cout<<"sut";
     std::cout<<"balix";
+    std::this_thread::sleep_for(std::chrono::seconds(5));
     std::cout<<"#dassaximizin salina";
     return 0;
 }
